58aeaebe882ff287cf552ea311b2957b22d28fea
Zevi Liao <jointjm@gmail.com>
2019-08-18 20:38:23 +0800

    styled-com

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
 import React, { Component } from "react";
-import styled, { css } from "styled-components";
-
-const Button = styled.button`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid palevioletred;
-  color: palevioletred;
-  margin: 0.5em 1em;
-  padding: 0.25em 1em;
-
-  ${props =>
-    props.primary &&
-    css`
-      background: palevioletred;
-      color: white;
-    `}
-`;
-
-const Container = styled.div`
-  text-align: center;
-`;
+import { Container, Button } from './styled-app';
 
 export default () => (
   <Container>
@@ -27,3 +7,4 @@ export default () => (
     <Button primary>Primary Button</Button>
   </Container>
 );
+
